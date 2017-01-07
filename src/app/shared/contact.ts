7d4b6a2fe6d27a516413ac3e7790fa6c49f17d14
goriunov <goriunov>
2017-01-08 11:38:47 +1300

    (aot app)

--- src/app/shared/contact.ts
+++
@@ -1,8 +0,0 @@
-export class contact{
-  constructor(
-    public body: string,
-    public from_email: string,
-    public to_email?: string,
-    public subject?:string,
-  ){}
-}
--- webpack.client.ts
+++
@@ -1,4 +0,0 @@
-// module.exports = {
-//   module: { loaders: [ { test: /\.ts$/, loaders: ['angular2-router-loader?loader=system'] } ] }
-// };
-// PM2 run in prod "start": "pm2 stop all && pm2 start ./dist/server/server.bundle.js -i max",
